ed5cd2b1b58e48cac2ff642a7b2503045fc784ba
Ludwig Warstedt <ludwig.warstedt@outlook.com>
2023-11-20 15:58:17 +0100

    asdasd

--- player.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-class Shotter(pygame.sprite.Sprite):
-    def __init__(self,positon = (250,250)):
-        super().__init__()
-        self.image = pygame.Surface([50,50])
-        self.image.fill((0,255,255))
-        self.rect = self.image.get_rect()
-        self.rect.center = positon
