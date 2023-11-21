1e73673511cfc2ae241b1fbb0e06b3edfe2b15d0
Ludwig Warstedt <ludwig.warstedt@outlook.com>
2023-11-21 15:49:32 +0100

    s

--- player1.py
+++ player1.py
@@ -7,7 +7,7 @@ class PLayer(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.image.load("PONG.png").convert_alpha()
         self.rect = self.image.get_rect()
-        self.rect.center = 500,350
+        self.rect.center = 600,350
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -19,5 +19,15 @@ class PLayer(pygame.sprite.Sprite):
             self.rect.y += 5
 
 
+    def out_of_bounds(self):
+        if self.rect.bottom >= 700:
+            self.rect.bottom = 699
+
+        if self.rect.top <= 0:
+            self.rect.y = 1
+
+
+
     def update(self) -> None:
         self.input()
+        self.out_of_bounds()
--- player2.py
+++ player2.py
@@ -11,12 +11,23 @@ class Player(pygame.sprite.Sprite):
 
     def input(self):
         keys = pygame.key.get_pressed()
-
+        mouse = pygame.mouse.get_pos()
+        print(mouse)
         if keys[pygame.K_w]:
             self.rect.y -= 5
 
         if keys[pygame.K_s]:
             self.rect.y += 5
 
+    def out_of_bounds(self):
+        if self.rect.bottom >= 700:
+            self.rect.bottom = 699
+
+        if self.rect.top <= 0:
+            self.rect.y = 1
+
+
+
     def update(self) -> None:
         self.input()
+        self.out_of_bounds()
